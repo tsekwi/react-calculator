d64015975ebaa6d4d0ca1a201cff9101b2d22d05
Tsekwi Simoyi <tsekwi@Tsekwis-MacBook-Pro.local>
2020-11-30 21:57:27 -0500

    cleaned up classes and styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Button from './Button';
 import './foundation.min.css';
 import './style.scss';
 
-
 const App = () => {
   const [value, setValue] = useState('');
 
@@ -18,54 +17,59 @@ const App = () => {
   const updateState = (e) => {
     setValue(e.target.value);
   }
-  
+
   const handleChange = (newValue) => {
     setValue(newValue);
   }
 
   return (
     <form onSubmit={ handleSubmit }>
-      <input type="text" value={ value } onChange={ updateState } style={{ width: '100%' }} />
-      <div className="grid-container">
-        <div className="item1" key="item-1">
-          <div className="grid-4">
-            {
-              calcSymbols.group_1.map(num => 
-                <Button 
-                num={ num } 
-                value={ value } 
-                change={ handleChange } 
-                keys={ num } />
-              )
-            }
-          </div>
+      <input type="text" placeholder="Start Calculating..." value={ value } onChange={ updateState } />
+      <div className="btn-container">
+        <div>
+            <div className="grid-4">
+              {
+                calcSymbols.group_1.map(num => 
+                  <Button 
+                  num={ num } 
+                  value={ value } 
+                  change={ handleChange } 
+                  classy="group_1" 
+                  keys={ num } />
+                )
+              }
+            </div>
         </div>
-        <div className="item2" key="item-2">
-          <div className="grid-3">
-            {
-              calcSymbols.group_2.map(num => 
-                <Button 
-                num={ num } 
-                value={ value } 
-                change={ handleChange } 
-                keys={ num } />
-              )
-            }
+        <div className="grid-container">
+          <div>
+            <div className="grid-3">
+              {
+                calcSymbols.group_2.map(num => 
+                  <Button 
+                  num={ num } 
+                  value={ value } 
+                  change={ handleChange } 
+                  classy="group_2" 
+                  keys={ num } />
+                )
+              }
+            </div>
           </div>
+          <div>
+            <div className="grid-1">
+              {
+                calcSymbols.group_3.map(num => 
+                  <Button 
+                  num={ num } 
+                  value={ value } 
+                  change={ handleChange } 
+                  classy="group_3" 
+                  keys={ num } />
+                )
+              }
+            </div>
+          </div> 
         </div>
-        <div className="item3" key="item-3">
-          <div className="grid-1">
-            {
-              calcSymbols.group_3.map(num => 
-                <Button 
-                num={ num } 
-                value={ value } 
-                change={ handleChange } 
-                keys={ num } />
-              )
-            }
-          </div>
-        </div> 
       </div>
     </form>
     );
